213e9a801b1414237f9053d4f2c5649da7718007
Kuba Janik <280170@student.pwr.edu.pl>
2025-07-16 17:11:54 +0200

    added

--- led-control/led-control.py
+++ led-control/led-control.py
@@ -8,10 +8,14 @@ ser = serial.Serial('COM5',19200,timeout=1)#add timeout
 
 print("PRESS 'SPACE' TO CHANGE LED STATE")
 
+
 while True:
     try:
         if keyboard.is_pressed('SPACE'):
+            time.sleep(0.1)  # debounce
             ser.write(b'1') #send binary 1 to arduino
+            while keyboard.is_pressed('SPACE'): {} #when space is held down, led wont flicker
+
             time.sleep(0.1) #debounce
 
         elif keyboard.is_pressed('esc'):
